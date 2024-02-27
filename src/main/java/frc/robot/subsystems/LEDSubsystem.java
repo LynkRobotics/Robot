b64d4f6a32d12585950b82231d86c58f56d1f08a
Jimmy McCosker <witherslayer67@gmail.com>
2024-02-27 08:12:47 -0500

    some LED work

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.RainbowAnimation;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -12,19 +13,30 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class LEDSubsystem extends SubsystemBase {
   /** Creates a new LEDSubsystem. */
   private CANdle m_candle = new CANdle(0, "rio");
+
+  public static class Color {
+    private final int R, G, B;
+    public Color(int r, int g, int b) {
+      R = r;
+      G = g;
+      B = b;
+    }
+  }
+  
   public LEDSubsystem() {
     m_candle.configBrightnessScalar(0.50);
     m_candle.configLEDType(LEDStripType.GRB);
     m_candle.configV5Enabled(true);
     m_candle.configLOSBehavior(true);
-    m_candle.setLEDs(191, 87, 0, 255, 0, 68);
   }
 
- 
+  
+  public void setRainbow(){
+    m_candle.animate(new RainbowAnimation(0.50, 0.5, 68, false, 8));
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    m_candle.setLEDs(191, 87, 0, 255, 0, 68);
-
   }
 }

a14ed437069094b5c37d25c374e57fb5dc81ba17
Jimmy McCosker <witherslayer67@gmail.com>
2024-07-29 21:06:26 -0400

    logging the Index subsystem

--- src/main/java/frc/robot/subsystems/IndexSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexSubsystem.java
@@ -7,6 +7,8 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import dev.doglog.DogLog;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -92,5 +94,9 @@ public class IndexSubsystem extends SubsystemBase {
     SmartDashboard.putBoolean("indexer/Have note", haveNote);
     SmartDashboard.putBoolean("indexer/Left sensor", leftIndexSensor.get());
     SmartDashboard.putBoolean("indexer/Right sensor", rightIndexSensor.get());
+
+    DogLog.log("Index/Have note", haveNote);
+    DogLog.log("Index/Left sensor", leftIndexSensor.get());
+    DogLog.log("Index/Right sensor", rightIndexSensor.get());
   }
 }
